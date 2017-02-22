680af89ccde86d7708cda0aa52fa0182d33abefb
Anton Shulgin <anton.shulgin@gmx.com>
2017-02-23 01:02:43 +0300

    rewritten slideshow logic

--- src/components/showcase/showcase.js
+++ src/components/showcase/showcase.js
@@ -2,55 +2,155 @@
 	// jshint esnext: true
 	'use strict';
 
+	var CLASS_NAME_SHOWCASE = 'showcase';
 	var CLASS_NAME_SHOWCASE_GALLERY = 'showcase-gallery';
 	var CLASS_NAME_SHOWCASE_IMAGE = 'showcase-image';
 	var CLASS_NAME_SHOWCASE_ACTIVE_IMAGE = 'showcase-image-active';
 
-	window.addEventListener('load', init, false);
+	var SELECTOR_SHOWCASE = '.' + CLASS_NAME_SHOWCASE;
+	var SELECTOR_SHOWCASE_GALLERY = '.' + CLASS_NAME_SHOWCASE_GALLERY;
+	var SELECTOR_SHOWCASE_IMAGE = '.' + CLASS_NAME_SHOWCASE_IMAGE;
+	var SELECTOR_SHOWCASE_ACTIVE_IMAGE = '.' + CLASS_NAME_SHOWCASE_ACTIVE_IMAGE;
 
-	function init() {
-		var gallery = getGallery();
+	var SLIDESHOW_INTERVAL = 6666;
 
-		if (!gallery) {
+	var internals = {};
+
+	window.addEventListener('load', initShowcase, false);
+
+	function initShowcase() {
+		setShowcase(findShowcase());
+		if (!getShowcase()) {
 			return;
 		}
-
-		var images = getImages(gallery);
-		var activeImageIndex = 0;
-
-		setActiveImage(activeImageIndex, images);
-		gallery.addEventListener('click', bumpActiveImage, false);
-		setInterval(bumpActiveImage, 6666);
-
-		function bumpActiveImage() {
-			activeImageIndex += 1;
-			if (!images[activeImageIndex]) {
-				activeImageIndex = 0;
-			}
-			setActiveImage(activeImageIndex, images);
+		setGallery(findGallery());
+		if (!getGallery()) {
+			return;
 		}
-
+		setImages(findImages());
+		if (!getImages()) {
+			return;
+		}
+		startSlideshow();
 	}
 
-	function setActiveImage(imageIndex, images) {
-		if (!images[imageIndex]) {
-			return;
-		}
-		for (var idx = 0; idx < images.length; idx += 1) {
-			images[idx].classList.remove(CLASS_NAME_SHOWCASE_ACTIVE_IMAGE);
-		}
-		images[imageIndex].classList.add(CLASS_NAME_SHOWCASE_ACTIVE_IMAGE);
+	function stopSlideshow() {
+		clearInterval(internals.slideshowInterval);
 	}
 
-	function getImages(gallery) {
-		if (!gallery) {
+	function startSlideshow() {
+		showImage(getActiveImageIndex());
+		internals.slideshowInterval = setInterval(showNextImage, SLIDESHOW_INTERVAL);
+	}
+
+	function showPreviousImage() {
+		showImage(getActiveImageIndex() - 1);
+	}
+
+	function showNextImage() {
+		showImage(getActiveImageIndex() + 1);
+	}
+
+	function showImage(imageIndex) {
+		if (!getImages()) {
 			return;
 		}
-		return gallery.querySelectorAll('.' + CLASS_NAME_SHOWCASE_IMAGE);
+		setActiveImageIndex(imageIndex);
+		clearActiveImages();
+		getImages()[getActiveImageIndex()].classList.add(CLASS_NAME_SHOWCASE_ACTIVE_IMAGE);
+	}
+
+	function getActiveImageIndex() {
+		return internals.activeImageIndex || 0;
+	}
+
+	function setActiveImageIndex(imageIndex) {
+		var isProperNumber = (Object.prototype.toString.call(imageIndex) === '[object Number]');
+		if (!isProperNumber) {
+			return;
+		}
+		var isFiniteNumber = isFinite(imageIndex);
+		if (!isFiniteNumber) {
+			return;
+		}
+		if (imageIndex >= getImages().length) {
+			internals.activeImageIndex = 0;
+			return;
+		}
+		if (imageIndex < 0) {
+			internals.activeImageIndex = getImages().length - 1;
+			return;
+		}
+		internals.activeImageIndex = imageIndex;
+	}
+
+	function clearActiveImages() {
+		var activeImages = findActiveImages();
+		if (!activeImages) {
+			return;
+		}
+		var idx;
+		for (idx = 0; idx < activeImages.length; idx += 1) {
+			activeImages[idx].classList.remove(CLASS_NAME_SHOWCASE_ACTIVE_IMAGE);
+		}
+	}
+
+	function findActiveImages() {
+		if (!getGallery()) {
+			return;
+		}
+		return getGallery().querySelectorAll(SELECTOR_SHOWCASE_ACTIVE_IMAGE);
+	}
+
+	function getImages() {
+		return internals.images;
+	}
+
+	function setImages(images) {
+		if (!images) {
+			return;
+		}
+		internals.images = images;
+	}
+
+	function findImages() {
+		if (!getGallery()) {
+			return;
+		}
+		return getGallery().querySelectorAll(SELECTOR_SHOWCASE_IMAGE);
 	}
 
 	function getGallery() {
-		return document.querySelector('.' + CLASS_NAME_SHOWCASE_GALLERY);
+		return internals.gallery;
+	}
+
+	function setGallery(gallery) {
+		if (!gallery) {
+			return;
+		}
+		internals.gallery = gallery;
+	}
+
+	function findGallery() {
+		if (!getShowcase()) {
+			return;
+		}
+		return getShowcase().querySelector(SELECTOR_SHOWCASE_GALLERY);
+	}
+
+	function getShowcase() {
+		return internals.showcase;
+	}
+
+	function setShowcase(showcase) {
+		if (!showcase) {
+			return;
+		}
+		internals.showcase = showcase;
+	}
+
+	function findShowcase() {
+		return document.querySelector(SELECTOR_SHOWCASE);
 	}
 
 })();

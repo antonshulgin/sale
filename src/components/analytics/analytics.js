f1d098f595def4078539d37cba33f45096c30060
Anton Shulgin <anton.shulgin@gmx.com>
2017-02-25 09:16:21 +0300

    added yandex.metrika

--- src/components/analytics/analytics.js
+++ src/components/analytics/analytics.js
@@ -6,4 +6,13 @@
 	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 	ga('create', 'UA-92538779-1', 'auto');
 	ga('send', 'pageview');
+
+	(function () {
+		(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter43049609 = new Ya.Metrika({ id:43049609, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
+		var metrikaPixel = document.createElement('noscript');
+		metrikaPixel.innerHTML = '<div><img src="https://mc.yandex.ru/watch/43049609" style="position:absolute; left:-9999px;" alt="" /></div>';
+		document.body.append(metrikaPixel);
+	})();
+
+
 })();
